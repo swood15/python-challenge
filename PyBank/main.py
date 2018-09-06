ca55ab7569b4c2e001a446fa93536029b3fe6769
swood15 <41810698+swood15@users.noreply.github.com>
2018-09-05 21:38:54 -0700

    Corrected error in calculations for max m/m inc/dec

--- PyBank/main.py
+++ PyBank/main.py
@@ -19,19 +19,17 @@ with open(csvpath, 'r', newline='', encoding="utf8") as csvfile:
         pnl.append(float(row[1]))
 
     change = [y-x for x,y in zip(pnl,pnl[1:])]
-    maxpnl = pnl.index(max(pnl))
-    minpnl = pnl.index(min(pnl))
+    maxchange = change.index(max(change))
+    minchange = change.index(min(change))
 
 def output():
     print("Financial Analysis")
     print("--------------------------------------------------")
     print(f"Total Months: {len(months)}")
     print(f"Total P&L: {locale.currency(sum(pnl), grouping=True)}")
-    print(f"Average Monthly Change: {locale.currency(numpy.mean(change), grouping=True)}")
-    # Greatest increase in profits refers to max(pnl) or max(change)?
-    print(f"Greatest Increase in P&L: {months[maxpnl]}, {locale.currency(pnl[maxpnl], grouping=True)}")
-    print(f"Greatest Decrease in P&L: {months[minpnl]}, {locale.currency(pnl[minpnl], grouping=True)}")
-
+    print(f"Average M/M Change: {locale.currency(numpy.mean(change), grouping=True)}")
+    print(f"Greatest M/M Increase in P&L: {months[maxchange+1]}, {locale.currency(change[maxchange], grouping=True)}")
+    print(f"Greatest M/M Decrease in P&L: {months[minchange+1]}, {locale.currency(change[minchange], grouping=True)}")
 print(output())
 
 output_path = 'financial_analysis.txt'
